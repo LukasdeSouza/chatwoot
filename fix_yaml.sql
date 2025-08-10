d36182d1ac02a15562cb0e996b45554c420d9882
LukasdeSouza <lucasdesouzasilva112@gmail.com>
2025-08-10 01:52:05 +0000

    fix for yaml of application name

---
+++ fix_yaml.sql
@@ -0,0 +1,5 @@
+UPDATE installation_configs SET serialized_value = '"--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\nvalue: OmniTech\n"' WHERE name = 'INSTALLATION_NAME';
+
+UPDATE installation_configs SET serialized_value = '"--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\nvalue: OmniTech\n"' WHERE name = 'BRAND_NAME';
+
+UPDATE installation_configs SET serialized_value = '"--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\nvalue: OmniTech API\n"' WHERE name = 'API_CHANNEL_NAME';
