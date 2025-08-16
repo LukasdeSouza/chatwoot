745dacba9c92ffdbfd8a8cf9ed7d4ee1690e2dfe
LukasdeSouza <lucasdesouzasilva112@gmail.com>
2025-08-16 00:14:46 +0000

    removing other integrations

--- app/javascript/dashboard/routes/dashboard/settings/inbox/ChannelList.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/ChannelList.vue
@@ -21,23 +21,20 @@ export default {
       return this.$store.getters['accounts/getAccount'](this.accountId);
     },
     channelList() {
-      const { apiChannelName, apiChannelThumbnail } = this.globalConfig;
-      return [
-        { key: 'website', name: 'Website' },
-        { key: 'facebook', name: 'Messenger' },
-        { key: 'whatsapp', name: 'WhatsApp' },
-        { key: 'sms', name: 'SMS' },
-        { key: 'email', name: 'Email' },
-        {
-          key: 'api',
-          name: apiChannelName || 'API',
-          thumbnail: apiChannelThumbnail,
-        },
-        { key: 'telegram', name: 'Telegram' },
-        { key: 'line', name: 'Line' },
-        { key: 'instagram', name: 'Instagram' },
-        { key: 'voice', name: 'Voice' },
-      ];
+        const { apiChannelName, apiChannelThumbnail } = this.globalConfig;
+        return [
+          { key: 'website', name: 'Website' },
+          // { key: 'whatsapp', name: 'WhatsApp' },
+          { key: 'sms', name: 'SMS' },
+          { key: 'email', name: 'Email' },
+          {
+            key: 'api',
+            name: apiChannelName || 'API',
+            thumbnail: apiChannelThumbnail,
+          },
+          { key: 'line', name: 'Line' },
+          { key: 'voice', name: 'Voice' },
+        ];
     },
     ...mapGetters({
       accountId: 'getCurrentAccountId',
